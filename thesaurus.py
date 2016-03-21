8f97bed638fce467ffdd89a198b270259957f909
Manwholikespie <rdominguez@me.com>
2016-03-21 11:18:22 -0500

    Update thesaurus.py, Update test.py
    Added new functions involving dictionaries. Made sure to optimize them as well. Added functions:
    
    cleanInput()
    findWord()
    findWordType()

--- thesaurus.py
+++ thesaurus.py
@@ -1,6 +1,4 @@
 """
-    First, we must develop an API for thesaurus.com, as all the current ones are crap.
-
     When searching a word (ex: "small"), the synonyms are grouped into three
     categories, differing in presentation by a change in the background color
     of their <span> object.
@@ -160,4 +158,152 @@ def findAntonyms(inputWord):
             if relevanceLevel == "relevant--3" or "relevant--2" or "relevant--1":
                 antonyms.append(str(word.text)) # using str() to remove unicode u''
 
-        return antonyms
+        return antonyms\
+
+def findWord(inputWord):
+    """
+        Combine both synonyms and antonyms into a large dictionary of mult.
+        itemtypes.
+
+        Now, I was considering having this just call the findRankedAntonyms and
+        findRankedSynonyms and building the dictionary this way... However that
+        is much slower. Here are the times as comparison:
+
+        # individual function calls
+                2.39 real         0.93 user         0.06 sys
+                2.24 real         0.94 user         0.05 sys
+                1.86 real         0.98 user         0.06 sys
+                average : 2.1633333333
+
+        # normal function
+                0.55 real         0.25 user         0.04 sys
+                0.37 real         0.24 user         0.03 sys
+                0.37 real         0.24 user         0.03 sys
+                average: 0.4033333333
+
+        as you can see, it is much faster to use the current html document than
+        calling a function that has to reload it each time.
+    """
+    # set up the soup of beauty
+    url = "http://www.thesaurus.com/browse/" + inputWord
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # check to see if there are actually synonyms for the entry.
+    errorTag = soup.select("#words-gallery-no-results")
+    if errorTag != []:
+        errorText = [item.text for item in errorTag][0]
+        print(errorText)
+    else:
+        # initialize a dictionary to hold all the antonyms
+        wordDict = {}
+
+        # synonyms
+        wordDict['syn3'] = []
+        wordDict['syn2'] = []
+        wordDict['syn1'] = []
+
+        #antonyms
+        wordDict['ant3'] = []
+        wordDict['ant2'] = []
+        wordDict['ant1'] = []
+        wordTags = soup.select("div#synonyms-0 section.container-info.antonyms div.list-holder ul.list li a span.text")
+
+        for word in wordTags:
+            relevanceLevel = word.parent.attrs["data-category"].rsplit("name\": \"")[1].rsplit("\",")[0]
+            if relevanceLevel == "relevant--3":
+                wordDict['ant3'].append(str(word.text)) # using str() to remove unicode u''
+                # print(word.text)
+            elif relevanceLevel == "relevant--2":
+                wordDict['ant2'].append(str(word.text))
+                # print(word.text)
+            elif relevanceLevel == "relevant--1":
+                wordDict['ant1'].append(str(word.text))
+                # print(word.text)
+
+        wordTags = soup.select("div#synonyms-0 div#filters-0 div.relevancy-block div.relevancy-list ul li a span.text")
+
+        for word in wordTags:
+            relevanceLevel = word.parent.attrs["data-category"].rsplit("name\": \"")[1].rsplit("\",")[0]
+            if relevanceLevel == "relevant-3":
+                wordDict['syn3'].append(str(word.text)) # using str() to remove unicode u''
+                # print(word.text)
+            elif relevanceLevel == "relevant-2":
+                wordDict['syn2'].append(str(word.text))
+                # print(word.text)
+            elif relevanceLevel == "relevant-1":
+                wordDict['syn1'].append(str(word.text))
+                # print(word.text)
+
+        return wordDict
+
+def findWordType(inputWord,type):
+    """
+    Same as findAntonyms or findSynonyms, however it returns them in an
+    organized dictionary.
+    """
+    type = cleanInput(type)
+
+    # set up the soup of beauty
+    url = "http://www.thesaurus.com/browse/" + inputWord
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # check to see if there are actually synonyms for the entry.
+    errorTag = soup.select("#words-gallery-no-results")
+    if errorTag != []:
+        errorText = [item.text for item in errorTag][0]
+        print(errorText)
+    else:
+        # initialize a dictionary to hold all the antonyms
+        wordDict = {}
+        if type == 'syn':
+            # synonyms
+            wordDict['syn3'] = []
+            wordDict['syn2'] = []
+            wordDict['syn1'] = []
+
+            wordTags = soup.select("div#synonyms-0 div#filters-0 div.relevancy-block div.relevancy-list ul li a span.text")
+
+            for word in wordTags:
+                relevanceLevel = word.parent.attrs["data-category"].rsplit("name\": \"")[1].rsplit("\",")[0]
+                if relevanceLevel == "relevant-3":
+                    wordDict['syn3'].append(str(word.text)) # using str() to remove unicode u''
+                    # print(word.text)
+                elif relevanceLevel == "relevant-2":
+                    wordDict['syn2'].append(str(word.text))
+                    # print(word.text)
+                elif relevanceLevel == "relevant-1":
+                    wordDict['syn1'].append(str(word.text))
+                    # print(word.text)
+        elif type == 'ant':
+            #antonyms
+            wordDict['ant3'] = []
+            wordDict['ant2'] = []
+            wordDict['ant1'] = []
+
+            wordTags = soup.select("div#synonyms-0 section.container-info.antonyms div.list-holder ul.list li a span.text")
+
+            for word in wordTags:
+                relevanceLevel = word.parent.attrs["data-category"].rsplit("name\": \"")[1].rsplit("\",")[0]
+                if relevanceLevel == "relevant--3":
+                    wordDict['ant3'].append(str(word.text)) # using str() to remove unicode u''
+                    # print(word.text)
+                elif relevanceLevel == "relevant--2":
+                    wordDict['ant2'].append(str(word.text))
+                    # print(word.text)
+                elif relevanceLevel == "relevant--1":
+                    wordDict['ant1'].append(str(word.text))
+                    # print(word.text)
+        elif type == "all":
+            wordDict = findWord(inputWord)
+        else:
+            print("ERROR: Not 'ant', 'syn', or 'all'.")
+            return {}
+
+        return wordDict
+
+def cleanInput(inputString):
+    inputString = inputString.lower()
+
+    return inputString
