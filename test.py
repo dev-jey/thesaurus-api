4769ca252fef9e14d56412f100f05585cc248cff
Manwholikespie <rdominguez@me.com>
2017-01-20 18:00:33 -0600

    Update thesaurus.py
    Added a new function, getWord().
    getWord() will grab all the data for word (save for its origin... sorry that's next time) across its multiple definitions and parts of speech.
    
    I hope to clean up the code next commit, and add some functions to organize the data output of getWord(). To the guy that forked me, thanks. Sorry for the messy function names.

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-from thesaurus import *
-
-origin = findOrigin("matter")
-print origin
--- thesaurus.py
+++ thesaurus.py
@@ -22,6 +22,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def findRankedSynonyms(inputWord,rank):
+    """Find the synonyms of a word according to a certain relevance level.
+
+    @param inputWord: The word you wish to search for
+    @param rank: An integer (1-3) referencing the relevance rank of the word,
+        where 3 is the most relevant and 1 is somewhat fitting.
+    @return: A list of synonyms that fit the relevance you searched for.
+    @rtype: List
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -57,6 +65,14 @@ def findRankedSynonyms(inputWord,rank):
         return wordDict['syn' + str(rank)]
 
 def findRankedAntonyms(inputWord,rank):
+    """Find the antonyms of a word according to a certain relevance level.
+
+    @param inputWord: The word you wish to search for
+    @param rank: An integer (1-3) referencing the relevance rank of the word,
+        where 3 is the most relevant and 1 is somewhat fitting.
+    @return: A list of synonyms that fit the relevance you searched for.
+    @rtype: List
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -90,6 +106,15 @@ def findRankedAntonyms(inputWord,rank):
         return wordDict['ant' + str(rank)]
 
 def findSynonyms(inputWord):
+    """Find all synonyms of a word.
+
+    @note: The returned list of synonyms is not sorted in any way, and is also
+        according to the first definition that thesaurus.com has set for the
+        word.
+    @param inputWord: The word you wish to search for
+    @return: A list of all synonyms for a given word.
+    @rtype: List
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -136,6 +161,15 @@ def findSynonyms(inputWord):
         return synonyms
 
 def findAntonyms(inputWord):
+    """Find all antonyms of a word.
+
+    @note: The returned list of antonyms is not sorted in any way, and is also
+        according to the first definition that thesaurus.com has set for the
+        word.
+    @param inputWord: The word you wish to search for
+    @return: A list of all antonyms for a given word.
+    @rtype: List
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -161,28 +195,40 @@ def findAntonyms(inputWord):
         return antonyms\
 
 def findWord(inputWord):
-    """
-        Combine both synonyms and antonyms into a large dictionary of mult.
-        itemtypes.
+    """Combine both synonyms and antonyms into a large dictionary of multiple itemtypes.
 
-        Now, I was considering having this just call the findRankedAntonyms and
-        findRankedSynonyms and building the dictionary this way... However that
-        is much slower. Here are the times as comparison:
+    @note: Now, I was considering having this just call the findRankedAntonyms and
+    findRankedSynonyms and building the dictionary this way... However that
+    is much slower. Here are the times as comparison:
+    # individual function calls
+            2.39 real         0.93 user         0.06 sys
+            2.24 real         0.94 user         0.05 sys
+            1.86 real         0.98 user         0.06 sys
+            average : 2.1633333333
+    # normal function
+            0.55 real         0.25 user         0.04 sys
+            0.37 real         0.24 user         0.03 sys
+            0.37 real         0.24 user         0.03 sys
+            average: 0.4033333333
 
-        # individual function calls
-                2.39 real         0.93 user         0.06 sys
-                2.24 real         0.94 user         0.05 sys
-                1.86 real         0.98 user         0.06 sys
-                average : 2.1633333333
+    as you can see, it is much faster to use the current html document than
+    calling a function that has to reload it each time.
 
-        # normal function
-                0.55 real         0.25 user         0.04 sys
-                0.37 real         0.24 user         0.03 sys
-                0.37 real         0.24 user         0.03 sys
-                average: 0.4033333333
+    @param inputWord: the word you wish to search for.
+    @return: A dict containing the synonyms and antonyms of a word's first
+        definition. Total elements:
+            Dict = {
+                # lists of synonyms according to rank
+                'syn1' = []
+                'syn2' = []
+                'syn3' = []
 
-        as you can see, it is much faster to use the current html document than
-        calling a function that has to reload it each time.
+                # lists of antonyms according to rank
+                'ant1' = []
+                'ant2' = []
+                'ant3' = []
+            }
+    @rtype: Dict
     """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
@@ -238,9 +284,15 @@ def findWord(inputWord):
         return wordDict
 
 def findWordType(inputWord,type):
-    """
-    Same as findAntonyms or findSynonyms, however it returns them in an
-    organized dictionary.
+    """Same as findAntonyms or findSynonyms, however it returns them in a dictionary organized by relevance.
+
+    @param inputWord: the word you wish to search for.
+    @param type: synonym or antonym - represented by 'syn' or 'ant'.
+    @return: a dictionary either of synonyms or antonyms. The output depends on
+        the type parameter...
+        If type is 'syn', there are elements: ['syn1'],['syn2'],['syn3'].
+        If type is 'ant', there are elements: ['ant1'],['ant2'],['ant3'].
+    @rtype: Dict.
     """
     type = cleanInput(type)
 
@@ -304,11 +356,48 @@ def findWordType(inputWord,type):
         return wordDict
 
 def cleanInput(inputString):
+    """makes strings lowercase and cleans them up for searching thesaurus.com
+
+    @note: Users shouldn't need to use this. This is mainly used within other
+        existing functions
+    @return: A cleaned up version of the inputted string.
+    @rtype: string
+    """
     inputString = inputString.lower()
 
     return inputString
 
 def findWordTotal(inputWord):
+    """Find all synonyms and antonyms for all of a given word's definitions.
+
+    @note: Usually thesaurus.com's default definition for a word contains
+        enough data to work with, however- in the offchance that you wish to use
+        other definitions of the word, you can use this to acquire said data.
+    @param inputWord: the word you wish to search for.
+    @return: A large dictionary whose top hierarchical elements are dictionaries
+        of named with integers from 0 to n-1, where n is the total amount of
+        definitions. Ex: findWordTotal(inputWord) returns a dict of:
+            {
+                '0' = {
+                    ['syn3'] = []
+                    ['syn2'] = []
+                    ['syn1'] = []
+                    ['ant3'] = []
+                    ['ant2'] = []
+                    ['ant1'] = []
+                    ['partOfSpeech'] = []
+                    ['meaning'] = []
+                }
+                '1' = {...} # the following are of similar content, save for
+                '2' = {...} # their meaning (and sometimes partOfSpeech)
+                '3' = {...}
+                '4' = {...}
+            }
+        Thus, there are 5 definitions for the word, each with their respective
+        meaning, partOfSpeech, synonyms, and antonyms- each of different relevance
+        rank of course.
+    @rtype: dict
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -386,6 +475,12 @@ def findWordTotal(inputWord):
         return wordDict
 
 def findExamples(inputWord):
+    """Find example sentences for a word.
+
+    @param inputWord: the word you wish to search for
+    @return: a list of example sentences for the inputted word.
+    @rtype: list
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -406,6 +501,12 @@ def findExamples(inputWord):
         return examples
 
 def findOrigin(inputWord):
+    """Find the origin of a word.
+
+    @param inputWord: the word you wish to search for.
+    @return: a long string containing the origin entry for the word-origin
+    @rtype: a long string.
+    """
     # set up the soup of beauty
     url = "http://www.thesaurus.com/browse/" + inputWord
     r = requests.get(url)
@@ -423,3 +524,124 @@ def findOrigin(inputWord):
         # for item in exampleTag:
         #     examples.append(str(item.text.replace("\n","").replace("\r","").replace("        ","").replace("    ","")))
         return origin
+
+def getWord(inputWord):
+    """Grab all data, across all definitions and parts of speech for a word.
+    A current downside is that the data isn't all that organized.
+    """
+    url = "http://www.thesaurus.com/browse/" + inputWord
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # check to see if there are actually synonyms for the entry.
+    errorTag = soup.select("#words-gallery-no-results")
+    if errorTag != []:
+        errorText = [item.text for item in errorTag][0]
+        print(errorText)
+        return None
+    else:
+        # No errors. Let the show go on.
+        pass
+
+    # initialize a dictionary to hold all the antonyms
+    wordDict = {}
+
+    # find the different parts of speech, and their meanings.
+    posTags = soup.select("div.mask a.pos-tab")
+    definitionCount =  len(posTags)
+    for item in posTags:
+        """The number of the item is stored in its data-id.
+        Its POS is in the <em.txt> below it.
+        Its meaning is in the <strong.ttl> also below it.
+        """
+        definitionNumber = str(item['data-id'])  # 0, 1, 2, etc.
+        wordDict[definitionNumber] = {}
+        wordDict[definitionNumber]['partOfSpeech'] = [
+            word.text for word in item.select('em')][0]
+        wordDict[definitionNumber]['meaning'] = [
+            word.text for word in item.select('strong')][0]
+
+    # Find the synonyms and antonyms for each definition.
+    for x in xrange(0,definitionCount):
+        # x corresponds to the current number of the definition.
+        for y in xrange(-3,3+1):
+            if y < 0:
+                wordDict[str(x)]['ant'+str(abs(y))] = []
+            elif y > 0:
+                wordDict[str(x)]['syn'+str(y)] = []
+
+        # synonyms
+        wordTags = soup.select("div#synonyms-"+str(x)+" div#filters-"+str(x)+" div.relevancy-block div.relevancy-list ul li a span.text")
+        for word in wordTags:
+            relevanceLevel = word.parent.attrs["data-category"].rsplit("name\": \"")[1].rsplit("\",")[0]
+            relevanceNumber = relevanceLevel[len(relevanceLevel)-1] # end char is the number
+            wordDict[str(x)]['syn'+relevanceNumber].append(str(word.text))
+
+        # antonyms
+        wordTags = soup.select("div#synonyms-"+str(x)+" section.container-info.antonyms div.list-holder ul.list li a span.text")
+        for word in wordTags:
+            relevanceLevel = word.parent.attrs["data-category"].rsplit("name\": \"")[1].rsplit("\",")[0]
+            relevanceNumber = relevanceLevel[len(relevanceLevel)-1] # end char is the number
+            wordDict[str(x)]['ant'+relevanceNumber].append(str(word.text))
+
+    return wordDict
+
+"""
+It has come to my attention that I have a lot of functions in this library,
+all with names that bear little resemblence to their functionality. In other
+words-- I need to rename them, possibly also re-work them in a way that makes
+it easier on people using this library.
+"""
+
+"""
+Let's think about what I want people to be able to do-- and thus which functions
+can stay the way they are...
+
+People need to:
+- get all the synonyms AND antonyms of a word.
+- get all the synonyms/antonyms of a word.
+- get synonyms/antonyms of a certain quality (relevance)
+- search a word in a specific grammatical function (noun, adverb, etc.)
+- search a word in a specific meaning (def1, def2, etc.)
+- get a word's origin and history.
+- get example sentences for a word.
+
+This should be good for now.
+"""
+
+"""
+The functions I need:
+Get ALL the data for a specific word (no matter how many requests).
+    - word complexity, relevancy, length
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
